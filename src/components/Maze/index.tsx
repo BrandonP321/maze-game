5c921792105cca29ee2fbcc2e831aa4e87901b0e
Brandon Phillips <brandon.phillips976@gmail.com>
2021-03-16 15:30:05 -0700

    added final solution to create entire maze

--- src/components/Maze/index.tsx
+++ src/components/Maze/index.tsx
@@ -26,7 +26,7 @@ interface cellProperties extends cellPositionProperties {
 }
 
 export default function Maze({ }: Props): ReactElement {
-    const [mazeWidth, setMazeWidth] = useState<number>(20); // height is same as width
+    const [mazeWidth, setMazeWidth] = useState<number>(30); // height is same as width
     const [mazeCells, setMazeCellsState] = useState<any[]>([]);
     const mazeCellsRef = useRef<any[]>([])
     const setMazeCells = (data: any) => {
@@ -50,6 +50,13 @@ export default function Maze({ }: Props): ReactElement {
         window.addEventListener('keydown', handleKeyPress)
     }, [])
 
+    useEffect(() => {
+        // check if player has reached end when they move
+        if (playerLocation.col === mazeWidth && playerLocation.row === mazeWidth) {
+            alert("You win")
+        }
+    }, [playerLocation])
+
     const generateCells: () => void = () => {
 
         let nums: cellPositionProperties[] = [];
@@ -72,7 +79,8 @@ export default function Maze({ }: Props): ReactElement {
         // createSimplePath(nums)
         // createRandomSimplePath(nums)
         // createSimpleLeftAndUpPath(nums);
-        createPathWithoutVisitingCellsTwice(nums);
+        // createPathWithoutVisitingCellsTwice(nums);
+        createPath(nums);
 
         setMazeCells(nums)
     }
@@ -183,7 +191,7 @@ export default function Maze({ }: Props): ReactElement {
             right: mazeWidth - 1,
             down: mazeWidth - 1,
             left: 0,
-            top: 0
+            up: 0
         }
 
 
@@ -234,21 +242,29 @@ export default function Maze({ }: Props): ReactElement {
                     currentCell.canMoveUp = true;
                     currentIndex -= mazeWidth;
                     cells[currentIndex].canMoveDown = true;
+                    remainingMoves.up--;
+                    remainingMoves.down++;
                     break;
                 case 'right':
                     currentCell.canMoveRight = true;
                     currentIndex += 1;
                     cells[currentIndex].canMoveLeft = true;
+                    remainingMoves.right--;
+                    remainingMoves.left++;
                     break;
                 case 'down':
                     currentCell.canMoveDown = true;
                     currentIndex += mazeWidth;
                     cells[currentIndex].canMoveUp = true;
+                    remainingMoves.down--;
+                    remainingMoves.up++;
                     break;
                 case 'left':
                     currentCell.canMoveLeft = true;
                     currentIndex -= 1;
                     cells[currentIndex].canMoveRight = true;
+                    remainingMoves.left--;
+                    remainingMoves.right++;
                     break;
             }
 
@@ -257,6 +273,135 @@ export default function Maze({ }: Props): ReactElement {
         }
     }
 
+    // create path from start cell to end cell
+    const createPath = (cells: any[]) => {
+        const validMoves = ['up', 'down', 'left', 'right'];
+
+        const remainingMoves = {
+            right: mazeWidth - 1,
+            down: mazeWidth - 1,
+            left: 0,
+            up: 0
+        }
+
+        const visitedCells: number[] = []
+
+        let currentIndex: any = 0;
+
+        let loopLimit = mazeWidth ** 2 - 2
+
+        let hasReachedEnd = false;
+
+        // iterate over cells' upper limit of all cells minus start and end
+        for (let i = 0; i < loopLimit; i++) {
+            const nextMoves = [] // valid moves for each iteration
+            let currentCell = cells[currentIndex]
+
+            // for each valid move (up down left and right) check if the move is valid
+            for (let move of validMoves) {
+                let nextCell = null;
+
+                // if we are in the end cell, don't create path from here
+                if (currentCell.isEnd) {
+                    hasReachedEnd = true;
+                    break;
+                }
+
+                switch (move) {
+                    case 'up':
+                        if (!currentCell.isTopRow) nextCell = cells[currentIndex - mazeWidth];
+                        break;
+                    case 'right':
+                        if (!currentCell.isRightCol) nextCell = cells[currentIndex + 1];
+                        break;
+                    case 'down':
+                        if (!currentCell.isBottomRow) nextCell = cells[currentIndex + mazeWidth];
+                        break;
+                    case 'left':
+                        if (!currentCell.isLeftCol) nextCell = cells[currentIndex - 1];
+                        break;
+                }
+
+                // if there is a cell in that direction that hasn't been visited
+                if (nextCell && !nextCell.hasBeenVisited) {
+                    // add that direction as a valid next move
+                    nextMoves.push(move);
+                }
+            }
+            
+            let randomMove;
+            // if user has reached end
+            if (hasReachedEnd) {
+                // generate completely random direction to move
+                const randomInt = Math.floor(Math.random() * nextMoves.length)
+                randomMove = nextMoves[randomInt]
+            } else {
+                // generate weighted random direction to move
+                randomMove = generateWeightedRandomMove(remainingMoves, nextMoves) // generates move based on weighted object of moves needed
+            }
+            
+            currentCell.hasBeenVisited = true;
+            // if there are no possible moves
+            if (nextMoves.length === 0) {
+                // move backwards in array of visited cells
+                currentIndex = visitedCells.pop();
+                loopLimit++
+                continue
+            }
+
+            // update cell's direction properties and move on to that cell
+            switch (randomMove) {
+                case 'up':
+                    currentCell.canMoveUp = true;
+                    currentIndex -= mazeWidth;
+                    cells[currentIndex].canMoveDown = true;
+                    remainingMoves.up--;
+                    remainingMoves.down++;
+                    break;
+                case 'right':
+                    currentCell.canMoveRight = true;
+                    currentIndex += 1;
+                    cells[currentIndex].canMoveLeft = true;
+                    remainingMoves.right--;
+                    remainingMoves.left++;
+                    break;
+                case 'down':
+                    currentCell.canMoveDown = true;
+                    currentIndex += mazeWidth;
+                    cells[currentIndex].canMoveUp = true;
+                    remainingMoves.down--;
+                    remainingMoves.up++;
+                    break;
+                case 'left':
+                    currentCell.canMoveLeft = true;
+                    currentIndex -= 1;
+                    cells[currentIndex].canMoveRight = true;
+                    remainingMoves.left--;
+                    remainingMoves.right++;
+                    break;
+            }
+
+
+            visitedCells.push(currentIndex);
+        }
+    }
+
+    
+    const generateWeightedRandomMove = (remainingMoves: any, possibleMoves: any[]) => {
+        let weightedArr = []
+        
+        for (let move of possibleMoves) {
+            // in the case that the remaining moves object has a value of 0, default that to 1
+            let movesCount = remainingMoves[move] === 0 ? 1 : remainingMoves
+
+            weightedArr.push(...new Array(movesCount).fill(move))
+        }
+        
+        // generate random index in weighted array
+        const randomInt = Math.floor(Math.random() * weightedArr.length);
+        return weightedArr[randomInt]
+    }
+    
     const handleKeyPress = (e: KeyboardEvent) => {
         // return early if player icon has not been loaded in yet
         if (!playerIcon) return
